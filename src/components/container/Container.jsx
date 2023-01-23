cabbb2bbc6f556a5ae16d15a19835fe51dad656a
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2023-01-23 23:40:20 +0530

    improvements

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,16 +1,7 @@
-import Navbar from "../Navbar/Navbar";
-import SearchBar from "../SearchBar/SearchBar";
-import UserDatails from "../UserDetails/UserDetails";
 import "./Container.css";
 
-const Container = () => {
-  return (
-    <div className="container">
-      <Navbar />
-      <SearchBar />
-      <UserDatails />
-    </div>
-  );
+const Container = ({ children }) => {
+  return <div className="container">{children}</div>;
 };
 
 export default Container;
